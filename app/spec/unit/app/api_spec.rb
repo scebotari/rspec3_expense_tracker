55d5875f02e73b472d062d20ad442a4a6402c6ac
Cebotari Sergei <cebotari.serghey@gmail.com>
2018-08-29 20:40:19 +0300

    Unit specs

---
+++ app/spec/unit/app/api_spec.rb
@@ -0,0 +1,121 @@
+require_relative '../../../app/api'
+require 'rack/test'
+
+module ExpenseTracker
+  RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    def expect_body
+      parsed = JSON.parse(last_response.body)
+      expect(parsed)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        let(:expense) { { 'some' => 'data' } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+        end
+
+        it 'returns the expense id' do
+          post '/expenses', JSON.generate(expense)
+
+          expect_body.to include('expense_id' => 417)
+        end
+
+        it 'responds with a 200 (OK)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the expense fails validation' do
+        let(:expense) { { 'some' => 'data' } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          expect_body.to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      let(:date) { '2017-06-12' }
+
+      context 'when expenses exist on the given date' do
+        let(:expenses) do
+          [
+            {
+              'expense_id' => 43,
+              'payee' => 'Starbucks',
+              'amount' => 5.75,
+              'date' => '2017-06-12'
+            },
+            {
+              'expense_id' => 44,
+              'payee' => 'McDonalds',
+              'amount' => 5.75,
+              'date' => '2017-06-12'
+            }
+          ]
+        end
+
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return(expenses)
+        end
+
+        it 'returns the expense records as JSON' do
+          get '/expenses/2017-06-12'
+          expect_body.to match_array(expenses)
+        end
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-12'
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when there are no expenses on the given date' do
+        let(:expenses) { [] }
+
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return(expenses)
+        end
+
+        it 'returns an empty array as JSON' do
+          get '/expenses/2017-06-12'
+          expect_body.to eq([])
+        end
+
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-12'
+          expect(last_response.status).to eq(200)
+        end
+      end
+    end
+  end
+end
